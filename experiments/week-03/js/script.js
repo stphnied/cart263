0f982194c1975cfbb03d30cd1211c9e4b8970d30
stphnieD <stphnie.dang@gmail.com>
2021-01-19 19:42:36 -0500

    W3: ResponsiveVoice

---
+++ experiments/week-03/js/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/*****************
+Week 03
+Stephanie Dang
+
+This week's topic and experiments includes :
+-Responsive voice
+******************/
+
+let phrase = `Would you like a cup of tea ?`;
+let saying = ``;
+
+function preload() {
+
+}
+
+
+function setup() {
+    createCanvas(500, 500);
+}
+
+
+function draw() {
+    background(255, 100, 150);
+
+    push();
+    textSize(32);
+    textAlign(CENTER);
+    text(saying, width / 2, height / 2);
+    pop()
+
+}
+
+function mousePressed() {
+
+    // UK English
+    responsiveVoice.speak("Would you like a cup of tea?", "UK English Male", {
+        onstart: showSpeaking,
+        onend: hideSpeaking
+    });
+
+    // French 
+    // responsiveVoice.speak("Comment ça va?", "French Female", {        
+    //     rate: 1.5,
+    //     volume: 1,
+    //     pitch: 1});
+}
+
+function showSpeaking() {
+    saying = phrase;
+}
+
+function hideSpeaking() {
+    saying = ``;
+}
