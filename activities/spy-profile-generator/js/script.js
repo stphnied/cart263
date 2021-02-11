3aad98cf776e02a2250c7e6f91c67ac2fae5789d
stphnieD <stphnie.dang@gmail.com>
2021-02-11 00:19:53 -0500

    E3: Improved the previous code

--- activities/spy-profile-generator/js/script.js
+++ activities/spy-profile-generator/js/script.js
@@ -2,54 +2,68 @@
 
 /*****************
 
-Spy profile generator
+Spy profile generator ++
 Stephanie Dang
 
 When the user first loads our program it will ask for their name in a text prompt.
 Once provided, the program will generate and save the user’s super secret spy profile using random JSON data to determine an alias,
 secret weapon, and password. When the user comes back later, they will need to enter their generated password to view their profile again.
 
-// password : disruption
 ******************/
 
+// Variables
+// Spy profile data displays
 let spyProfile = {
     name: `**REDACTED**`,
     alias: `**REDACTED**`,
     secretWeapon: `**REDACTED**`,
     password: `**REDACTED**`
 };
+
+// Var to store JSON data
 let tarotData;
 let objectData;
 let instrumentData;
 
+
+// Constants
+// URL to data
+const
+    TAROT_DATA_URL = `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`;,
+    OBJECT_DATA_URL= `https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`;,
+    INSTRUMENT_DATA_URL= `https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`;
+// Colors code
 const
     COLOR_GREEN = `#39FF14`,
     COLOR_BLACK = `#000000`;
 
-// preload()
-// Description of preload
-function preload() {
 
-    tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
-    objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
-    instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+// preload()
+// Loads all the JSON data
+function preload() {
+    tarotData = loadJSON(TAROT_DATA_URL);
+    objectData = loadJSON(OBJECT_DATA_URL);
+    instrumentData = loadJSON(INSTRUMENT_DATA_URL);
 }
 
 // setup()
-// Creating canvas
-// Displays the generated profile if there is one
-// Users has to enter correct password
-// If wrong do nothing
+// Creating canvas and handles loadings profile data
+// Generating new profile if none
 function setup() {
+    // creates canvas
     createCanvas(windowWidth, windowHeight);
-
+    // load data
     let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+    // Check if there's a data to load
     if (data !== null) {
+        // Asks for password
+        // If matches, displays profile
         let password = prompt(`What's your code?`);
         if (password === data.password) {
             spyProfile = data;
         }
     }
+    // If not, generate a new profile
     else {
         generateSpyProfile();
     }
@@ -57,9 +71,8 @@ function setup() {
 
 
 // draw()
-// Description of draw()
+// Displays current profile
 function draw() {
-
     background(COLOR_BLACK);
 
     // string template
@@ -79,12 +92,17 @@ function draw() {
     pop();
 }
 
-function generateSpyProfile() {
-    let card = random(tarotData.tarot_interpretations);
 
+// Generates spy profile from JSON data
+function generateSpyProfile() {
+    // Ask for agent's name and stores it
     spyProfile.name = prompt(`Identify yourself, Agent`, `name`);
+    // random instrument
     spyProfile.alias = `the ${random(instrumentData.instruments)}`;
+    // random object
     spyProfile.secretWeapon = random(objectData.objects);
+    // random tarot card as password
+    let card = random(tarotData.tarot_interpretations);
     spyProfile.password = random(card.keywords);
 
     //Saving generated profile
