ed00dd8ac8cca9099a0155d9a32e35cffc291bed
stphnieD <stphnie.dang@gmail.com>
2021-03-18 19:11:47 -0400

    A6: Done

--- activities/raving-redactionist/js/script.js
+++ activities/raving-redactionist/js/script.js
@@ -1,11 +1,33 @@
 /**
-Title of Project
-Author Name
+Activity 06: Raving Redactionist
+Stephanie Dang
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+We will create a webpage of text with some passages “redacted” (covered in colored bars).
 */
-
 "use strict";
 
-// Code goes here
+$(`span`).addClass(`top-secret`);
+$(`.top-secret`).on(`click`,redact);
+setInterval(revelation, 500);
+
+
+// Calls attemptReveal function for all span
+function revelation() {
+    $(`span`).each(attemptReveal)
+}
+
+// Reveals the secret information
+function attemptReveal() {
+    let num = Math.random(0, 1);
+
+    if (num < 0.1) {
+        $(this).removeClass(`redacted`);
+        $(this).addClass(`revealed`);
+    }
+}
+
+// Redactes the secret info on click
+function redact() {
+        $(this).removeClass(`revealed`);
+        $(this).addClass(`redacted`);
+}
