f533218cd20b59c094af81db8c26e2e183c0a4a4
stphnieD <stphnie.dang@gmail.com>
2021-03-31 02:36:56 -0400

    P2: Working on selection menu
    - Added some fancy CSS
    - List of available cato characters

--- projects/project-02/prototype/js/claw-machine.js
+++ projects/project-02/prototype/js/claw-machine.js
@@ -3,7 +3,15 @@ Project 2: Anything
 Stephanie Dang.
 
 This script is dedicated to the claw-machine functionality/interactivity.
+
+For the prototype:
+As of now, the user is able draag the coin and insert it inside the coin slot to activate the joystick and button
+Once they click on the button, it will drop down&up the claw and returns to its initial position.
+The joystick and button are will be disabled after that. 
+
 */
+
+// VARIABLES
 
 // Claw machine parts
 let clawHandle = $(`#claw-machine-handle`);
@@ -14,28 +22,38 @@ let jsLeft = false;
 let btnDown = false;
 
 // Money
-let walletAmount = 0;
-let coin25, coin1, coin2;
 const clawMPrice = 0.25;
+let walletAmount = 0;
 let insertedCoins = 0;
+let amountPaid = false;
+let coin25, coin1, coin2;
 
+// Adding absolute class to all the claw-machine parts
 $(`#claw-machine img`).addClass(`claw-machine-parts`);
+
+
+
+/*/////////////////////////////////////////////////////////////////////////////////
+COINS
+*/ /////////////////////////////////////////////////////////////////////////////////
 
 // Adding values to the coins 
 // Coin-0 -> 0.25c
 // Coin-1 -> 1$
 // Coin-2 -> 2$
-$(`#coin-0`).attr(`value`, `0.25`);
-$(`#coin-1`).attr(`value`, `1`);
-$(`#coin-2`).attr(`value`, `2`);
+$(`.coin-0`).attr(`value`, `0.25`);
+$(`.coin-1`).attr(`value`, `1`);
+$(`.coin-2`).attr(`value`, `2`);
 
 // Coins are draggable
-$(`#coin-0`).draggable({});
+$(`.coin-0`).draggable({
+    revert: "valid"
+});
 
 // Insert coins here
 $(`#claw-machine-coin-slot`).droppable({
     drop: function (event, ui) {
-        $(ui.draggable).remove();
+        // $(ui.draggable).remove();
         $(ui.draggable).hide({
             effect: `blind`,
             duration: 500
@@ -45,43 +63,56 @@ $(`#claw-machine-coin-slot`).droppable({
         insertedCoins += $(ui.draggable).attr(`value`);
         // Check if they add the exact amount to play
         if (insertedCoins == clawMPrice) {
-            console.log("hi");
+            amountPaid = true;
         }
     }
 });
 
 // Reset coins
 $(`#claw-machine-btn-reset`).on(`click`, function (event) {
+    console.log("clicked it!");
+    $(`.coin-01`).show("blind");
 
 })
 
-// Down button
+
+/*/////////////////////////////////////////////////////////////////////////////////
+CLAW MACHINE DOWN BUTTON (↓) 
+*/ /////////////////////////////////////////////////////////////////////////////////
+
 // change the scale on Y of the button to create a "click" animation
 // Control the clawHandle up and down movement, brings back to initial position
 clawBtnDown.on({
     mousedown: function () {
-        btnDown = true;
-        $(this).css(`transform`, `scaleY(0.8)`);
-        clawHandle.animate({
-            top: "+=100",
-            animation: "easein"
-        }, "slow")
-        clawHandle.animate({
-            top: "0",
-            animation: "easeout"
-        }, "slow")
-        clawHandle.animate({
-            left: 0,
-            animation: "easeinout"
-        }, 3000)
+        if (amountPaid) {
+            btnDown = true;
+            $(this).css(`transform`, `scaleY(0.8)`);
+            clawHandle.animate({
+                top: "+=100",
+                animation: "easein"
+            }, "slow")
+            clawHandle.animate({
+                top: "0",
+                animation: "easeout"
+            }, "slow")
+            clawHandle.animate({
+                left: 0,
+                animation: "easeinout"
+            }, 3000)
+        }
     },
     mouseup: function () {
         $(this).css(`transform`, `scaleY(1)`);
+        insertedCoins = 0;
+        btnDown = false;
+        amountPaid = false;
     }
 });
 
 
-// Joystick
+/*/////////////////////////////////////////////////////////////////////////////////
+Joystick
+*/ /////////////////////////////////////////////////////////////////////////////////
 
 // On mouse events
 // mousedown: set the jsDown bool to true --> allows rotation
@@ -100,7 +131,7 @@ clawJoystick.on({
         let mouseX = event.pageX - this.offsetLeft;
         let mousePosCenter = 660;
 
-        if (insertedCoins == clawMPrice) {
+        if (amountPaid) {
             if (jsDown && !btnDown) {
                 // to the right
                 if (mouseX > mousePosCenter) {
