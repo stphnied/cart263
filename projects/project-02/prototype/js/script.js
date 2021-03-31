24796c8fbc4493280dc9c41f7b8e9108f168500e
stphnieD <stphnie.dang@gmail.com>
2021-03-31 05:00:55 -0400

    P2: Added JSON files
    Implemented JSON cats url into code

--- projects/project-02/prototype/js/script.js
+++ projects/project-02/prototype/js/script.js
@@ -1,11 +1,48 @@
 /**
-Project 2: Anything
+Project 2: Gachapyon
 Stephanie Dang.
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+For the prototype: 
 */
 
 "use strict";
 
-// Code goes here
+/*/////////////////////////////////////////////////////////////////////////////////
+MENU
+*/ /////////////////////////////////////////////////////////////////////////////////
+
+// Changing CSS cursor to default
+$(`.card`).css(`cursor`, `default`);
+
+// Select buttons event listener
+$(`#menu button`).on("click", function (event) {
+
+    // Hiding menu and showing instruction section
+    $(`#menu`).css(`display`, `none`);
+    $(`#instruction`).css(`display`, `block`);
+
+    // Get the parent's id of selected cat
+    let catId = $(this).parent().parent().attr(`id`);
+    // Gets the cats JSON image url depending on the selected cat
+    $.getJSON("assets/data/cats.json", function (catsData) {
+        switch (catId) {
+            case `tofu-cat`:
+                $(`#user`).attr(`src`, catsData.cats.tofu.url)
+                break;
+            case `lilo-cat`:
+                $(`#user`).attr(`src`, catsData.cats.lilo.url)
+                break;
+            case `kosper-cat`:
+                $(`#user`).attr(`src`, catsData.cats.kosper.url)
+                break;
+        }
+    });
+})
+
+// Hides instruction and show claw-machine section
+$(`#instruction button`).on("click", function (event) {
+    $(`#instruction`).css(`display`, `none`);
+    $(`#claw-machine`).css(`display`, `block`);
+
+
+})
