39dfc8b068dc23ba3569f41ad0a280c41349004c
stphnieD <stphnie.dang@gmail.com>
2021-04-17 22:22:03 -0400

    P2: Added p5js library

---
+++ projects/project-02/final/js/mini-game.js
@@ -0,0 +1,17 @@
+/**
+Project 2: Gachapyon
+Stephanie Dang.
+
+This script is dedicated to the minigames functionalities.
+
+- Uses p5js
+- 
+*/
+
+function preload() {}
+
+function setup(){
+    createCanvas(750,750);
+}
+
+function draw() {}
