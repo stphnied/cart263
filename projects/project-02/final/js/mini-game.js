d04bebd5a21d980baa9e8cddae232c4f5deda9a7
stphnieD <stphnie.dang@gmail.com>
2021-04-21 00:03:52 -0400

    P2: Working on the minigame(s)
    Trying to figure out how p5js works again...

--- projects/project-02/final/js/mini-game.js
+++ projects/project-02/final/js/mini-game.js
@@ -8,10 +8,34 @@ This script is dedicated to the minigames functionalities.
 - 
 */
 
-function preload() {}
+// Variables
+let minigameCanvas;
+let catsData;
+let catImg;
+const
+    CATS_URL = `assets/data/cats.json`
+    NUM_CATS = 3;
 
-function setup(){
-    createCanvas(750,750);
+
+
+function preload() {
+    catsData = loadJSON(CATS_URL);
 }
 
-function draw() {}
+function setup() {
+    minigameCanvas = createCanvas(750, 750);
+    minigameCanvas.parent("#mini-games");
+    catImg = loadImage(catsData.cats.tofu.paw_url); 
+}
+
+function draw() {
+    background(127);
+    displayUser();
+    
+}
+
+
+function displayUser() {
+     imageMode(CENTER);
+     image(catImg, mouseX, mouseY, 100, 100);
+}
